2811bf55fc6471871a50513218884633a8e2d99f
rejaul-islam1 <rejaul.hero@gmail.com>
2022-04-18 17:16:57 +0600

    read me file edited

--- src/components/Home/CheckOut/CheckOut.js
+++ src/components/Home/CheckOut/CheckOut.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './CheckOut.css'
 import { useParams } from 'react-router-dom';
 
 const CheckOut = () => {
     const { serviceId } = useParams();
     return (
-        <div className='text-center mt-5'>
+        <div className='text-center checkout-container'>
             <h2>Welcome to Check Out</h2>
             <h2> Please Check Out: {serviceId}</h2>
         </div>
--- src/components/Share/Footer/Footer.js
+++ src/components/Share/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div className='text-white bg-dark py-5 text-center mt-4'>
+        <div className='footer-container text-white bg-dark py-5 text-center mt-4'>
             <h2 className=' carousel-title'>Smith & Click</h2>
             <p><small>Copyright © {year}</small></p>
         </div>
